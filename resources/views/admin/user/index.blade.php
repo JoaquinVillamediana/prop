4a8314f465a01ef511b8b265d546b615caf51d26
Gonzalo Brizuela <gonzalobrizuelaortiz@icloud.com>
2020-08-04 20:38:36 +0000

    Pagina default iniciada

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.nav')
+@section('content')
+<div class="container-prueba">
+    <h1>
+    Pureba usuarios
+    </h1>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/frontend/home.index');
 });
+
+Route::get('/admin', function () {
+    return redirect('/admin/user');
+});
+
 
 Auth::routes();
 
