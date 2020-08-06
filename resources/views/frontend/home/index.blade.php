b796b19350640767a601c24c64f690de3701f0f8
Joaquin <Joaquin@trenders.com.ar>
2020-08-06 16:59:49 +0000

    Cambios en cards y buscador

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
 <link rel="stylesheet" href="/css/frontend/home.css">
+<link rel="stylesheet" href="/css/frontend/properties.css">
 <section id="main">
   <div id="carousel" class="carousel slide" data-ride="carousel" data-pause="false">
 
@@ -27,15 +28,15 @@
               <p> Publica tu propiedad en 5 minutos y consigue que nuestros mas de 500.000 usuarios la vean.
               </p>
             </div>
-            <div class="col-md-5 col-12"><input placeholder="Ubicacion:" type="text" name="text" id="text">
+            <div class="col-md-6 col-12"><input placeholder="Ubicacion:" type="text" name="text" id="text">
             </div>
-            <div class="col-md-2 col-12 ">
+            <div class="col-md-2 search-option col-12 ">
               <select name="type" id="type">
                 <option value="1">Alquiler</option>
                 <option value="2">Venta</option>
               </select>
             </div>
-            <div class="col-md-2 col-12">
+            <div class="col-md-2 search-option last col-12">
               <select name="building" id="building">
                 <option selected value="0">Cualquiera</option>
                 <option value="1">Casa</option>
@@ -43,7 +44,7 @@
                 <option value="1">Casa</option>
               </select>
             </div>
-            <div class="col-md-3 col-12">
+            <div class="col-md-2 col-12 text-right">
               <button class="btn btn-search"><i class="fas fa-search"></i></button>
             </div>
             <div class="col-12">
@@ -56,45 +57,7 @@
             </div>
           </div>
         </div>
-        
-        {{-- <div class="container">
-          <div class="row">
-            <div class="overlay">
-              <div class="container mt-4">
-                <h1>Bienvenido a CuloProp</h1>
-                <p> Publica tu propiedad en 5 minutos y consigue que nuestros mas de 500.000 usuarios la vean.
-                </p> --}}
 
-
-        {{-- <div class="container">
-                  <div class="row row-search"> --}}
-        {{-- <div class="col-12  text-center">
-              <a href="" class="btn btn-search">BUSCADOR</a>
-            </div> --}}
-        {{-- <div class="col-md-5 col-12"><input placeholder="Ubicacion:" type="text" name="text" id="text">
-                    </div>
-                    <div class="col-md-2 col-12 ">
-                      <select name="type" id="type">
-                        <option value="1">Alquiler</option>
-                        <option value="2">Venta</option>
-                      </select>
-                    </div>
-                    <div class="col-md-2 col-12">
-                      <select name="building" id="building">
-                        <option selected value="0">Cualquiera</option>
-                        <option value="1">Casa</option>
-                        <option value="1">Casa</option>
-                        <option value="1">Casa</option>
-                      </select>
-                    </div>
-                    <div class="col-md-3 col-12">
-                      <button class="btn btn-search"><i class="fas fa-search"></i></button>
-                    </div> --}}
-        {{-- <div class="col-12 text-center">
-              <a href="#" class="btn btn-searcher">
-                Buscador Avanzado
-              </a>
-            </div> --}}
       </div>
     </div>
 
@@ -125,20 +88,31 @@
     <div class="row">
       <div class="col-md-4 mb-4">
         <div class="card">
-          <img src="images/index/home1.jpg" class="card-img-top" alt="...">
+          <div class="image">
+            <img src="images/index/home1.jpg" class="card-img-top" alt="...">
+
+            <div class="row row-caracs">
+              
+                {{-- <div class="col-2"> --}}
+                  <span class="characteristic">3<i class="fas fa-home"></i></span>
+                {{-- </div> --}}
+                {{-- <div class="col-2"> --}}
+                  <span class="characteristic">1<i class="fas fa-toilet"></i></span>
+                {{-- </div> --}}
+              </div>
+
+          </div>
+
           <div class="card-body">
-            <div class="badge mb-2">
-              <span class="badge badge-warning"> 4 dormintorios </span>
-              <span class="badge badge-info"> 3 baños </span>
-            </div>
             <h5 class="card-title mb-0">Prop 1</h5>
 
             <p class="card-text">4 baños y un par de boludeces.</p>
             <a href="#" class="btn btn-primary">Más información</a>
           </div>
         </div>
-
       </div>
+
+
       <div class="col-md-4 mb-4">
         <div class="card">
           <img src="images/index/home2.jpg" class="card-img-top" alt="...">
