3bb2915a9dc61a264a46d83d935f94d808df463c
Gonzalo Brizuela <gonzalobrizuelaortiz@icloud.com>
2020-08-15 01:18:21 +0000

    Edicion de footer

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -5,19 +5,44 @@
   <div class="container">
       <div class="row text-center">
           <div class="col-12 col-lg">
-              <a href="#"> Preguntas frecuentes </a>
+          <div class="row">
+              <a href="#"> Vender </a>
+              </div>
+              <div class="row">
+              <a href="#"> Alquilar </a>
+              </div>
+
+              <div class="row">
+              <a href="#"> Ayuda </a>
+              </div>
+              <div class="row">
+              <a href="#"> Publicidad </a>
+              </div>
+              <div class="row">
+              <a href="#"> Noticias </a>
+              </div>
           </div>
           <div class="col-12 col-lg">
               <a href="#"> Contactanos </a>
           </div>
           <div class="col-12 col-lg">
-              <a href="#"> Prensa </a>
+          <a href="#"> Preguntas frecuentes </a>
+         
+         
           </div>
           <div class="col-12 col-lg">
-              <a href="#"> Terminos y condiciones </a>
+             
+          <a href="https://wbosoftware.com/"> Conocé más sobre W.B.O. Software </a>
           </div>
           <div class="col-12 col-lg">
-              <a href="#"> XD </a>
+          <a class="media-link" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                        <a class="media-link" href="https://twitter.com/"><i
+                            class="fab fa-twitter"></i></a>
+                        <a class="media-link" href="https://www.instagram.com/"><i
+                            class="fab fa-instagram"></i></a>
+                            <a class="media-link" href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+                            
+
           </div>
       </div>
   </div>
